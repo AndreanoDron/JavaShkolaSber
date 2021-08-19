e53517d900387772bf0b36352633a0848c0a02e3
AndreanoDron <dron.rkrp@mail.ru>
2021-08-19 23:15:14 +0300

    Sortirovka

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -10,8 +10,9 @@ public class BinarySearch {
         int[] arr = {10, 20, 30, 40, 50, 60, 70, 80};
         System.out.println("Ищем число " + number + " в массиве!!!");
         int x = getBinarySearch(number, arr);
-        System.out.println("X= " + x);
-        if (x > 0) {
+        // для проверки System.out.println("Возврашаемое число по индексу = " + x);
+        System.out.println("Возврашаемое число по индексу = " + x);
+        if (x >= 0  ) {
             System.out.println(number + " Такое число есть в массиве!!!");
 
         } else {
@@ -20,77 +21,12 @@ public class BinarySearch {
     }
 
     private static int getBinarySearch(int number, int[] arr) {
-        // System.out.println(Arrays.binarySearch(arr,number));
+
         return Arrays.binarySearch(arr, number);
     }
 
 
-//        int minIndex = arr[0];
-//        int maxIndex = arr[arr.length - 1];
-//        System.out.println(minIndex);
-//        System.out.println(max);
 
-//        BinarySearch_Rec(arr, minIndex, maxIndex, number);
-
-//        int arr[] = {2, 3, 4, 10, 40};
-//        int n = arr.length;
-//        int x = 3;
-//
-//        int result = binarySearch(arr, 0, n - 1, x);
-//        if (result == -1) {
-//            System.out.println("Element not present");
-//        } else {
-//            System.out.println("Element found at index " +
-//                    result);
-//        }
-
-
-//    static int binarySearch(int[] array, int left, int right, int key) {
-//        int mid = left + (right - left) / 2;
-//        if (array[mid] == key)
-//            return mid;
-//        else if (array[mid] > key) {
-//            return binarySearch(array, left, mid - 1, key);
-//        } else {
-//            return binarySearch(array, mid + 1, right, key);
-//        }
-//    }
-
-//    private static int binarySerch(int number, int[] arr, int min, int max) {
-//        int position;
-//
-//        // для начала найдем индекс среднего элемента массива
-//        position = min + (max - min) / 2;
-//      if (arr[position]==number){
-//          return position;
-//      }else if (arr[position]>number){
-//          return+1;
-//      }else {
-//          return -1;
-//      }
-
-//    Теперь посмотрим на рекурсивную реализацию:
-//
-//    public static int recursiveBinarySearch(int arr[], int firstElement, int lastElement, int elementToSearch) {
-//
-//        // условие прекращения
-//        if (lastElement >= firstElement) {
-//            int mid = firstElement + (lastElement - firstElement) / 2;
-//
-//            // если средний элемент - целевой элемент, вернуть его индекс
-//            if (arr[mid] == elementToSearch)
-//                return mid;
-//
-//            // если средний элемент больше целевого
-//            // вызываем метод рекурсивно по суженным данным
-//            if (arr[mid] > elementToSearch)
-//                return recursiveBinarySearch(arr, firstElement, mid - 1, elementToSearch);
-//
-//            // также, вызываем метод рекурсивно по суженным данным
-//            return recursiveBinarySearch(arr, mid + 1, lastElement, elementToSearch);
-//        }
-//
-//        return -1;
 }
 
 
