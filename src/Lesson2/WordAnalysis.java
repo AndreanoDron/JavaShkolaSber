ff670fab505b20d7696df9d24dac274512adb73c
AndreanoDron <dron.rkrp@mail.ru>
2021-09-19 22:06:02 +0300

    Figure

---
+++ src/Lesson2/WordAnalysis.java
@@ -0,0 +1,21 @@
+package Lesson2;
+
+import java.util.Comparator;
+
+public class WordAnalysis<S>  implements Comparator<String> {
+
+    @Override
+    public int compare(String o1, String o2) {
+        if (o1.length() == o2.length()) {
+            return 0;
+        } else if (o1.length() > o2.length()) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+
+
+
+}
